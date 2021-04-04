adef89764aab63ad84fadf506b9152428a3d2557
colin <cjmaret@gmail.com>
2021-04-04 18:30:17 +0700

    fix: review issues

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,10 @@
 
 let editButton = document.querySelector(".profile__button-edit");
 
-let popUp = document.querySelector(".edit-profile");
+let popUp = document.querySelector(".modal");
 
 function displayPopUp() {
-    popUp.setAttribute("style", "display: block");
+    popUp.setAttribute("style", "display: flex");
 };
 
 editButton.addEventListener("click", displayPopUp);
@@ -15,7 +15,7 @@ editButton.addEventListener("click", displayPopUp);
 
 //Close Pop Up//
 
-let xButton = document.querySelector(".edit-profile__close-icon");
+let xButton = document.querySelector(".modal__close-icon");
 
 function closePopUp() {
     popUp.removeAttribute("style", "display: block");
@@ -31,9 +31,9 @@ let title = document.querySelector(".profile__title");
 
 let subtitle = document.querySelector(".profile__subtitle");
 
-let newName = document.querySelector(".edit-box__input_title");
+let newName = document.querySelector(".edit-box__input_type_title");
 
-let newDescription = document.querySelector(".edit-box__input_subtitle");
+let newDescription = document.querySelector(".edit-box__input_type_subtitle");
 
 let saveButton = document.querySelector(".edit-box__button");
 
@@ -52,11 +52,11 @@ saveButton.addEventListener("click", addInfo);
 
 // Heart Black on Active // 
 
-let heart = document.querySelectorAll(".image-grid__heart");
+let heart = document.querySelectorAll(".image-card__heart");
 
 
 for (let i = 0; i < heart.length; i++) {
     heart[i].addEventListener("click", function() {
-        heart[i].classList.toggle("black-heart");
+        heart[i].classList.toggle("image-card__heart_black-heart");
     });
  }

8c0c48d5ea3e4e9ea19cdacca3bd36b2f57d44cb
colin <cjmaret@gmail.com>
2021-04-15 19:18:07 +0700

    feat: add card, remove card, original 6 card add

--- scripts/script.js
+++ scripts/script.js
@@ -1,53 +1,226 @@
-// Open Pop Up //
-
-let title = document.querySelector(".profile__title");
-
-let subtitle = document.querySelector(".profile__subtitle");
-
-let inputName = document.querySelector(".edit-box__input_type_title");
-
-let inputDescription = document.querySelector(".edit-box__input_type_subtitle");
-
-let editButton = document.querySelector(".profile__button-edit");
-
-let popUp = document.querySelector(".modal");
 
 
-function displayPopUp() {
+// Open Edit Pop Up //
+
+const title = document.querySelector(".profile__title");
+
+const subtitle = document.querySelector(".profile__subtitle");
+
+const inputName = document.querySelector(".edit-box__input_type_title");
+
+const inputDescription = document.querySelector(".edit-box__input_type_subtitle");
+
+const editButton = document.querySelector(".profile__button-edit");
+
+const editPopUp = document.querySelector(".modal_type_edit");
+
+const imageGrid = document.querySelector(".image-grid");
+
+
+
+// Initial Load of 6 Image Cards // 
+
+const initialCards = [
+    {
+        name: "Yosemite Valley",
+        link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+        name: "Lake Louise",
+        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+        name: "Bald Mountains",
+        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+        name: "Latemar",
+        link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+        name: "Vanoise National Park",
+        link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    },
+    {
+        name: "Lago di Braies",
+        link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+];
+
+initialCards.forEach(card => {
+
+    const initialCardTemplate = document.querySelector("#image-card-template").content;
+    const initialCardElement = initialCardTemplate.querySelector(".image-card").cloneNode(true);
+
+    initialCardElement.querySelector(".image-card__image").src = card.link;
+    initialCardElement.querySelector(".image-card__title").textContent = card.name;
+
+
+    imageGrid.append(initialCardElement);
+
+    initialCardElement.querySelector(".image-card__heart").addEventListener("click", function (evt) {
+        evt.target.classList.toggle("image-card__heart_liked");
+    });
+
+    initialCardElement.querySelector(".image-card__trash").addEventListener("click", function (evt) {
+        evt.target.closest(".image-card").remove();
+    });
+
+
+     // Opening Picture Popup //
+
+     const modalImageTemplate = document.querySelector("#modal-image-template").content;
+     const modalImageElement = modalImageTemplate.querySelector(".image-expand").cloneNode(true);
+ 
+     document.querySelector(".image-card__image").addEventListener("click", function (evt) {
+ 
+         modalImageElement.querySelector(".image-expand__image").src = evt.target.src;
+         modalImageElement.querySelector(".image-expand__title").textContent = evt.target.textContent;
+ 
+         modalImageElement.classList.add("modal__open");
+     });
+
+
+
+});
+
+
+
+
+
+
+// Open Edit Pop Up //
+
+function displayEditPopUp() {
     inputName.value = title.textContent;
     inputDescription.value = subtitle.textContent;
 
-    popUp.classList.add("modal_open");
+    editPopUp.classList.add("modal_open");
 };
 
-editButton.addEventListener("click", displayPopUp);
+editButton.addEventListener("click", displayEditPopUp);
 
 
-//Close Pop Up//
+//Close Edit Pop Up//
 
-let xButton = document.querySelector(".modal__close-icon");
+const editCloseButton = document.querySelector(".modal__close-icon_type_edit");
 
-function closePopUp() {
-    popUp.classList.remove("modal_open");
+function closeEditPopUp() {
+    editPopUp.classList.remove("modal_open");
 }
 
-xButton.addEventListener("click", closePopUp);
+editCloseButton.addEventListener("click", closeEditPopUp);
 
 
-// Set Name and Description //
 
-let saveForm = document.querySelector(".edit-box");
+// Set Profile Name and Description //
 
-function addInfo(evt) {
+const saveProfileForm = document.querySelector(".edit-box_type_profile");
+
+function addProfileInfo(evt) {
     evt.preventDefault();
 
     title.textContent = inputName.value;
     subtitle.textContent = inputDescription.value;
 
-    closePopUp();
+    closeEditPopUp();
 
 }
 
-saveForm.addEventListener("submit", addInfo);
+saveProfileForm.addEventListener("submit", addProfileInfo);
 
 
+
+
+// open Add Popup //
+
+const addPopUp = document.querySelector(".modal_type_add");
+const imageTitle = document.querySelector(".image-card__title");
+const imageLink = document.querySelector(".image-card__link");
+const inputImageTitle = document.querySelector(".edit-box__input_type_image-title");
+const inputImageLink = document.querySelector(".edit-box__input_type_image-link");
+const addButton = document.querySelector(".profile__button-add");
+
+function displayAddPopUp() {
+    inputImageTitle.value = "";
+    inputImageLink.value = "";
+
+    addPopUp.classList.add("modal_open");
+}
+
+addButton.addEventListener("click", displayAddPopUp);
+
+
+
+// close Add Popup //
+
+const addCloseButton = document.querySelector(".modal__close-icon_type_add");
+
+function closeAddPopUp() {
+    addPopUp.classList.remove("modal_open");
+}
+
+addCloseButton.addEventListener("click", closeAddPopUp);
+
+
+
+
+// Add Image Card // 
+
+
+const saveImageForm = document.querySelector(".edit-box_type_image");
+
+
+function addImageCard(evt) {
+    evt.preventDefault();
+
+    const imageCardTemplate = document.querySelector("#image-card-template").content;
+    const imageCardElement = imageCardTemplate.querySelector(".image-card").cloneNode(true);
+
+    imageCardElement.querySelector(".image-card__image").src = inputImageLink.value;
+    imageCardElement.querySelector(".image-card__title").textContent = inputImageTitle.value;
+
+    imageGrid.prepend(imageCardElement);
+
+    closeAddPopUp();
+
+
+    // Change Heart Color //
+
+    imageCardElement.querySelector(".image-card__heart").addEventListener("click", function (evt) {
+        evt.target.classList.toggle("image-card__heart_liked");
+    });
+
+
+    // Remove Image Card // 
+
+    imageCardElement.querySelector(".image-card__trash").addEventListener("click", function (evt) {
+        evt.target.closest(".image-card").remove();
+    });
+
+
+
+}
+
+saveImageForm.addEventListener("submit", addImageCard);
+
+
+
+
+
+
+
+
+
+
+
+    // // Remove Image Card // 
+
+    // const trashButton = document.querySelectorAll(".image-card__trash");
+
+    // trashButton.forEach(button => {
+    //     button.addEventListener("click", function () {
+    //         const imageCardToRemove = button.closest(".image-card");
+    //         imageCardToRemove.remove();
+    //     });
+    // });

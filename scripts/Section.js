730d9a72188737892b1d5246676412c3801b29cc
cjmaret <77926563+cjmaret@users.noreply.github.com>
2021-06-10 17:17:43 +0700

    section

--- scripts/Section.js
+++ scripts/Section.js
@@ -11,19 +11,12 @@ export default class Section {
     }
 
     addItem(element) {
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-
->>>>>>> feat/popup-class-and-refactor-index-popups
         this._container.append(element);
-=======
         if (element === "newCardElement") {
             this._container.prepend(element);
         } else {
             this._container.append(element);
         }
->>>>>>> Stashed changes
     }
 
 }
